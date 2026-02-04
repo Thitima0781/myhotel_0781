b1f2751ac10f97db0d17057f0f9c9f11f4ff9ce9
Thitima_0781 <thitimachueakaeo.kratae@gmail.com>
2026-02-04 11:29:58 +0700

    final

---
+++ myhotel_0781.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 04, 2026 at 05:29 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `myhotel_0781`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guestbook`
+--
+
+CREATE TABLE `guestbook` (
+  `id` int(4) NOT NULL,
+  `name` varchar(65) NOT NULL,
+  `email` varchar(65) NOT NULL,
+  `comment` longtext NOT NULL,
+  `datetime` varchar(65) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `guestbook`
+--
+
+INSERT INTO `guestbook` (`id`, `name`, `email`, `comment`, `datetime`) VALUES
+(1, 'Thitima', '67050781@kmitl.ac.th', 'good hotel', '04022026'),
+(2, 'T2MHK', 'mmm@gmail.com', '&#3648;&#3619;&#3636;&#3656;&#3604; &#3611;&#3633;&#3591;&#3611;&#3640;&#3619;&#3636;&#3648;&#3618;&#3656;!', '26-02-04 05:19:35'),
+(3, 'T2MHK', 'mmm@gmail.com', '&#3648;&#3619;&#3636;&#3656;&#3604; &#3611;&#3633;&#3591;&#3611;&#3640;&#3619;&#3636;&#3648;&#3618;&#3656;!', '26-02-04 05:20:25'),
+(4, '', '', '', '26-02-04 05:20:29'),
+(5, '', '', '', '26-02-04 05:20:32'),
+(6, 'minnie', 'mnm@gmail.com', 'goodddd', '26-02-04 05:20:52');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `guestbook`
+--
+ALTER TABLE `guestbook`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `guestbook`
+--
+ALTER TABLE `guestbook`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
